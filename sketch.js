e4e43814e318ad2ab48d4ea42a8018b1e87450a6
Malcolm <crummynz@gmail.com>
2015-11-21 14:26:52 +0100

    final build

--- sketch.js
+++ sketch.js
@@ -5,29 +5,31 @@
 // Example 1-1: stroke and fill
 
 function setup() {
-  createCanvas(1280, 720);
+  createCanvas(320, 480);
   noStroke();
   fill(150);
-  for (var x = 0; x < width; x += 10) {
-    for (var y = 0; y < height; y += 10) {
+
+}
+
+function draw() {
+  var boxSize = 6;
+  for (var x = 0; x < width; x += boxSize) {
+    for (var y = 0; y < height; y += boxSize) {
       var distance = distanceToCenter(x, y);
       var colour = getColour(distance);
       fill(colour);
-      rect(x, y, 10, 10);
-      console.log(x, y, colour, distance);
+      rect(x, y, boxSize, boxSize);
     }
   }
 }
 
-function draw() {
-
-}
-
 function distanceToCenter(x, y) {
-  return Math.sqrt((x*x)+(y*y));
+  return (x*x)+(y*y);
 }
 
 function getColour(distance) {
-  var colour = Math.sin(distance + frameCount) * 255;
-  return color(colour, colour, colour);
+  var r = Math.sin((distance - frameCount * 1000) * .0001);
+  var g = Math.sin((distance - frameCount * 1000) * .0002);
+  var b = Math.sin((distance - frameCount * 1000) * .0004);
+  return color((r+1)/2 * 255, (g+1)/2 * 255, (b+1)/2 * 255);
 }
